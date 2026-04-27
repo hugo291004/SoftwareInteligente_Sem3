f22e1d6a22f7580053380bc5efb9bed581f711fe
xLorenz29 <renzo.29.lp@gmail.com>
2026-04-26 20:58:40 -0500

    Mejora del agente ProductOwnerAgent

--- src/ProductOwnerAgent.java
+++ src/ProductOwnerAgent.java
@@ -1,33 +1,66 @@
+/*
+    Para ejecutar desde la carpeta base en POWERSHELL
+    Compilar los .class desde src a out
+    • javac -cp ".;..\lib\jade.jar" -d ..\out *.java
+
+    Desde la carpeta base
+    • java -cp ".;lib\jade.jar;out" jade.Boot -gui ProductOwner:ProductOwnerAgent
+*/
+
 import jade.core.Agent;
 import jade.wrapper.AgentController;
 import jade.wrapper.ContainerController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProductOwnerAgent extends Agent {
+
+    // Backlog simulado
+    private List<Object[]> backlog = new ArrayList<>();
 
     protected void setup() {
         System.out.println("Product Owner iniciado...");
 
+        initBacklog();
         createTaskAgents();
+    }
+
+    // Inicializa tareas
+    private void initBacklog() {
+        backlog.add(new Object[]{1, 5, "high"});
+        backlog.add(new Object[]{2, 3, "medium"});
+        backlog.add(new Object[]{3, 8, "high"});
+        backlog.add(new Object[]{4, 2, "low"});
+        backlog.add(new Object[]{5, 4, "medium"});
     }
 
     private void createTaskAgents() {
 
         ContainerController cc = getContainerController();
 
-        for (int i = 1; i <= 4; i++) {
+        for (Object[] task : backlog) {
             try {
 
-                String agentName = "TaskAgent-" + i;
+                int taskId = (int) task[0];
+                int effort = (int) task[1];
+                String priority = (String) task[2];
+
+                String agentName = "TaskAgent-" + taskId;
 
                 AgentController agent = cc.createNewAgent(
                         agentName,
                         "TaskAgent",
-                        null
+                        new Object[]{taskId, effort, priority}
                 );
 
                 agent.start();
 
-                System.out.println("Creado: " + agentName);
+                System.out.println(
+                    "Creado: " + agentName +
+                    " -> effort=" + effort +
+                    ", priority=" + priority
+                );
 
             } catch (Exception e) {
                 e.printStackTrace();
